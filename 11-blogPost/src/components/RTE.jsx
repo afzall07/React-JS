8c1eca57f0ea6a357ee59fab67d89fca3334ae1d
afzall07 <afjalmalik75000@gmail.com>
2024-11-13 20:20:25 +0530

    blog-web

---
+++ 11-blogPost/src/components/RTE.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            apiKey="pohjce5gyyt43b6ozrk5u5rj7i3g9ygdmxmjas72y3t4pqms"
+            init={{
+              plugins: [
+                // Core editing features
+                "anchor",
+                "autolink",
+                "charmap",
+                "codesample",
+                "emoticons",
+                "image",
+                "link",
+                "lists",
+                "media",
+                "searchreplace",
+                "table",
+                "visualblocks",
+                "wordcount",
+                // Your account includes a free trial of TinyMCE premium features
+                // Try the most popular premium features until Nov 23, 2024:
+                "checklist",
+                "mediaembed",
+                "casechange",
+                "export",
+                "formatpainter",
+                "pageembed",
+                "a11ychecker",
+                "tinymcespellchecker",
+                "permanentpen",
+                "powerpaste",
+                "advtable",
+                "advcode",
+                "editimage",
+                "advtemplate",
+                "ai",
+                "mentions",
+                "tinycomments",
+                "tableofcontents",
+                "footnotes",
+                "mergetags",
+                "autocorrect",
+                "typography",
+                "inlinecss",
+                "markdown",
+                // Early access to document converters
+                "importword",
+                "exportword",
+                "exportpdf",
+              ],
+              toolbar:
+                "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+              tinycomments_mode: "embedded",
+              tinycomments_author: "Author name",
+              mergetags_list: [
+                { value: "First.Name", title: "First Name" },
+                { value: "Email", title: "Email" },
+              ],
+              ai_request: (request, respondWith) =>
+                respondWith.string(() =>
+                  Promise.reject("See docs to implement AI Assistant")
+                ),
+              exportpdf_converter_options: {
+                format: "Letter",
+                margin_top: "1in",
+                margin_right: "1in",
+                margin_bottom: "1in",
+                margin_left: "1in",
+              },
+              exportword_converter_options: { document: { size: "Letter" } },
+              importword_converter_options: {
+                formatting: {
+                  styles: "inline",
+                  resets: "inline",
+                  defaults: "inline",
+                },
+              },
+            }}
+            initialValue={defaultValue}
+          />
+        )}
+      />
+    </div>
+  );
+}
